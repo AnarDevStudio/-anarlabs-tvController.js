ff2d7e1d97562f732c680d8c49c3229109d25cd5
AnarLabs <anardevstudio@gmail.com>
2026-01-29 18:54:48 +0400

    files are created and project is compiled

---
+++ dist/index.d.ts
@@ -0,0 +1,4 @@
+export * from './controller';
+export * from './focus';
+export * from './events';
+//# sourceMappingURL=index.d.ts.map
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export * from './controller';
+export * from './focus';
+export * from './events';
